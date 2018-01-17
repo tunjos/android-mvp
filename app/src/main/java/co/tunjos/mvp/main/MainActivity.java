e197d4622f419f9b0a2e84a140a4d9d5fe510a75
Tunji Olu-Taiwo <tunjos@tunjos.co>
2018-01-17 17:42:11 +0100

    Move MainActivity to main package

--- app/src/main/java/co/tunjos/mvp/main/MainActivity.java
+++ app/src/main/java/co/tunjos/mvp/main/MainActivity.java
@@ -1,4 +1,4 @@
-package co.tunjos.mvp;
+package co.tunjos.mvp.main;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import co.tunjos.mvp.R;
 
 public class MainActivity extends AppCompatActivity {
 
